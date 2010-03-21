b64e584407028d1c069953fe8f2ba6fa2b83c23c
Dmitry Medvinsky <dmedvinsky@gmail.com>
2010-03-21 23:14:12 +0300

    Add config
    It's a bit ugly but OK for now. :)

---
+++ Config.h
@@ -0,0 +1,40 @@
+#ifndef CONFIG_H
+#define CONFIG_H
+
+#include <iostream>
+#include <fstream>
+#include <string>
+
+using namespace std;
+
+
+class Config
+{
+public:
+    static Config* getInstance();
+
+    // options
+    bool show_calendar;
+
+private:
+    static Config* _instance;
+    static void Destroy();
+
+    template <class T> bool fromString(T& t, const string& s);
+    string strip(string const& str);
+
+    std::ifstream file;
+
+protected:
+    explicit Config();
+    ~Config();
+
+    void getDefaults();
+    bool getFile();
+    void readFile();
+    void parseLine(string line);
+    void addOption(string var, string val);
+
+};
+
+#endif
